febcd9f32c8b90e34b69601d9189624575c96192
aajmorgan6 <120695223+aajmorgan6@users.noreply.github.com>
2024-06-13 14:53:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     # py_modules=["touchid"],
-    version="1.2.0",
+    version="2.0.0",
     description="Access the Touch ID sensor",
     long_description="`touchid` provides an easy to use API for accessing the Touch ID sensor via Apple's LocalAuthentication framework",
     keywords=["touch-id", "LocalAuthentication", "LAContext"],
